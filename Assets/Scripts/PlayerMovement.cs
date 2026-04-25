641c16824cacc1c24c40dc29073ad5c6bde63a06
DDeraJJ-Deluxe <wownotevenbro@gmail.com>
2026-04-24 19:45:36 -0700

    Tilemaps highkey tilting me

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,12 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
+
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float speed = 1f;
+
     void Start()
     {
         
@@ -11,6 +15,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xInput = Input.GetAxisRaw("Horizontal");
+        Vector3 movement = new Vector3(xInput, 0, 0);
+
+        movement.Normalize();
+
+        transform.Translate(movement * speed * Time.deltaTime);
+
     }
 }
